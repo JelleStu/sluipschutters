ce29646d2e153fb24995853726eadd8ff521845e
Jelle <jellestukstette@hotmail.com>
2018-03-05 22:00:36 +0100

    4e pull. Filmpje hitler compleet.

--- app/src/main/java/nl/jellestukstette/sluipschutterssoundboard/MainActivity.java
+++ app/src/main/java/nl/jellestukstette/sluipschutterssoundboard/MainActivity.java
@@ -8,13 +8,14 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
     Button bt;
-    Button bthitler;
+    Button btstinkbek;
+    Button bthoerenpaard;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        bt = (Button) findViewById(R.id.btnViezeVuile);
-        final MediaPlayer mp = MediaPlayer.create(this, R.raw.sample);
+        bt = (Button) findViewById(R.id.btnHeeHitler);
+        final MediaPlayer mp = MediaPlayer.create(this, R.raw.heehitler);
         bt.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view){
@@ -22,12 +23,23 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        bthitler = (Button) findViewById(R.id.btnHeeHitler);
-        final MediaPlayer mp2 = MediaPlayer.create(this, R.raw.heehitler);
-        bthitler.setOnClickListener(new View.OnClickListener(){
+
+        btstinkbek = (Button) findViewById(R.id.btnStinkBek);
+        final MediaPlayer mp3 = MediaPlayer.create(this, R.raw.stinkbek);
+        btstinkbek.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view){
-                mp2.start();
+                mp3.start();
+
+            }
+        });
+
+        bthoerenpaard = (Button) findViewById(R.id.btnHoerenPaard);
+        final MediaPlayer mp4 = MediaPlayer.create(this, R.raw.hoerenpaard);
+        bthoerenpaard.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                mp4.start();
                 // only change for push
             }
         });

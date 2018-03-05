4445b9c2969770a48b2597529fd1c23c8450fb87
JelleStu <jelso99@gmail.com>
2018-03-05 14:43:25 +0100

    Initial commit

---
+++ app/src/main/java/nl/jellestukstette/sluipschutterssoundboard/MainActivity.java
@@ -0,0 +1,24 @@
+package nl.jellestukstette.sluipschutterssoundboard;
+
+import android.media.MediaPlayer;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button bt;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        bt = (Button) findViewById(R.id.btnViezeVuile);
+        final MediaPlayer mp = MediaPlayer.create(this, R.raw.sample);
+        bt.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                mp.start();
+            }
+        });
+    }
+}
